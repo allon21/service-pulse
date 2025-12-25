3f98d3aaea78ed455ee8ebc13609f99aee15779c
allon21 <anthon.chub@yandex.ru>
2025-12-25 23:34:25 +0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/servicepulse/servicepulse/ServicepulseApplication.java
@@ -0,0 +1,13 @@
+package com.servicepulse.servicepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicepulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicepulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicepulse/servicepulse/ServicepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicepulse.servicepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
